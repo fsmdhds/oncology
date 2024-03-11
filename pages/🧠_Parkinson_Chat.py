fc10b813c4081a906c0d6807f273f80bd1aae0c8
David Liebovitz <DavidL@northwestern.edu>
2024-03-11 17:10:35 -0500

    password added

--- pages/🧠_Parkinson_Chat.py
+++ pages/🧠_Parkinson_Chat.py
@@ -8,6 +8,41 @@ from langchain.chains import RetrievalQA
 # from langchain.embeddings import OpenAIEmbeddings
 from langchain_community.embeddings import OpenAIEmbeddings
 
+st.set_page_config(page_title='Neurology Chats', layout = 'centered', page_icon = "💬", initial_sidebar_state = 'auto')    
+
+def check_password2():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["password"] == st.secrets["password"]:
+            st.session_state["password_correct"] = True
+            # del st.session_state["password"]  # don't store password
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        # First run, show input for password.
+        if st.secrets["use_docker"] == "False":
+            st.text_input(
+                "Password", type="password", on_change=password_entered, key="password"
+            )
+            st.write("*Please contact David Liebovitz, MD if you need an updated password for access.*")
+            return False
+        else:
+            st.session_state["password_correct"] = True
+            return True
+    elif not st.session_state["password_correct"]:
+        # Password not correct, show input + error.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        st.error("😕 Password incorrect")
+        return False
+    else:
+        # Password correct.
+        return True
+
 llm = ChatOpenAI(openai_api_key=st.secrets['OPENAI_API_KEY'], model_name ="gpt-3.5-turbo", temperature=0)
 
 # Load the FAISS database
@@ -25,15 +60,17 @@ st.title("Parkinson's Disease Question Answering")
 
 # Get user input
 
-user_role = st.radio("What is your role?", ["Patient", "Neurologist", "Other"], horizontal=True)
-if user_role == "Other":
-    user_role = st.text_input("Enter your role:")
+if st.secrets["use_docker"] == "True" or check_password2():
 
-query = st.text_input("Ask a question about Parkinson's Disease:")
+    user_role = st.radio("What is your role?", ["Patient", "Neurologist", "Other"], horizontal=True)
+    if user_role == "Other":
+        user_role = st.text_input("Enter your role:")
 
-final_query = f'As a {user_role}, so please use appropriate terms, {query}'
+    query = st.text_input("Ask a question about Parkinson's Disease:")
 
-# If the user enters a query, get the answer
-if query:
-    answer = qa_chain(query)
-    st.write(answer["result"])
+    final_query = f'As a {user_role}, so please use appropriate terms, {query}'
+
+    # If the user enters a query, get the answer
+    if query:
+        answer = qa_chain(query)
+        st.write(answer["result"])
