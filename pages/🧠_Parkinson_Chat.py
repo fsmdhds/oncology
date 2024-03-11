29fdb2d994430aeca1904aac5664ea821e39bc58
David Liebovitz <DavidL@northwestern.edu>
2024-03-11 17:14:52 -0500

    updated spinner

--- pages/🧠_Parkinson_Chat.py
+++ pages/🧠_Parkinson_Chat.py
@@ -43,17 +43,6 @@ def check_password2():
         # Password correct.
         return True
 
-llm = ChatOpenAI(openai_api_key=st.secrets['OPENAI_API_KEY'], model_name ="gpt-3.5-turbo", temperature=0)
-
-# Load the FAISS database
-embeddings = OpenAIEmbeddings(openai_api_key=st.secrets['OPENAI_API_KEY'],model="text-embedding-3-large")
-vectorstore = FAISS.load_local("parkinson_disease.faiss", embeddings)
-
-# # Set up the OpenAI LLM
-# llm = OpenAI(temperature=0)
-
-# Create the question-answering chain
-qa_chain = RetrievalQA.from_chain_type(llm=llm, retriever=vectorstore.as_retriever(), chain_type="stuff")
 
 # Streamlit app
 st.title("Parkinson's Disease Question Answering")
@@ -61,6 +50,19 @@ st.title("Parkinson's Disease Question Answering")
 # Get user input
 
 if st.secrets["use_docker"] == "True" or check_password2():
+    
+    with st.spinner("Preparing Databases..."):
+        llm = ChatOpenAI(openai_api_key=st.secrets['OPENAI_API_KEY'], model_name ="gpt-3.5-turbo", temperature=0)
+
+        # Load the FAISS database
+        embeddings = OpenAIEmbeddings(openai_api_key=st.secrets['OPENAI_API_KEY'],model="text-embedding-3-large")
+        vectorstore = FAISS.load_local("parkinson_disease.faiss", embeddings)
+
+    # # Set up the OpenAI LLM
+    # llm = OpenAI(temperature=0)
+
+    # Create the question-answering chain
+    qa_chain = RetrievalQA.from_chain_type(llm=llm, retriever=vectorstore.as_retriever(), chain_type="stuff")
 
     user_role = st.radio("What is your role?", ["Patient", "Neurologist", "Other"], horizontal=True)
     if user_role == "Other":
@@ -72,5 +74,6 @@ if st.secrets["use_docker"] == "True" or check_password2():
 
     # If the user enters a query, get the answer
     if query:
-        answer = qa_chain(query)
-        st.write(answer["result"])
+        with st.spinner("Fomulating Answer..."):
+            answer = qa_chain(query)
+            st.write(answer["result"])
